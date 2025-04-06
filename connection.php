cc8493c43997db29daf97f4f777b496fdfb0677f
sepans <sepans@sepans.com>
2025-04-06 23:23:47 +0330

    .

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysql_connect('127.0.0.1', 'root', 'letmein11')
+$con = mysql_connect('', '', '')
     or die('Could not connect: ' . mysql_error());
 
 mysql_select_db('theuse') or die('Could not select database');
