90530c177a41891ac683ac247ff57d42457422c4
sepans <sepans@sepans.com>
2025-04-06 23:53:10 +0330

    update mysql remove select db

--- connection.php
+++ connection.php
@@ -3,6 +3,5 @@
 $con = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'))
     or die('Could not connect: ' . mysql_error());
 
-mysql_select_db('theuse') or die('Could not select database');
 
 ?>
